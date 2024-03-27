8aacc466174b723129e05c3237278a52426eeec1
Alex Baranowski <alex@euro-linux.com>
2024-03-27 18:14:06 +0900

    Frozen autocommit 2024-03-27 18:14:06.188561

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # google-guest-agent - main branch
 
-This repository contains frozen state of the repository.
+This repository contains the frozen state of the repository.
 Its history is cleared. It is used only for static code
 analysis with Codacy.
 
-Commit Time: 1710826679
+Commit Time: 1711530846
--- metadata/metadata.go
+++ metadata/metadata.go
@@ -240,7 +240,7 @@ func (a *Attributes) UnmarshalJSON(b []byte) error {
 		SSHKeys               string      `json:"ssh-keys"`
 		TwoFactor             string      `json:"enable-oslogin-2fa"`
 		SecurityKey           string      `json:"enable-oslogin-sk"`
-		RequireCerts          string      `json:"require-oslogin-certificates"`
+		RequireCerts          string      `json:"enable-oslogin-certificates"`
 		WindowsKeys           WindowsKeys `json:"windows-keys"`
 		WSFCAddresses         string      `json:"wsfc-addrs"`
 		WSFCAgentPort         string      `json:"wsfc-agent-port"`
@@ -506,10 +506,15 @@ func (c *Client) do(ctx context.Context, cfg requestConfig) (*http.Response, err
 		return resp, fmt.Errorf("error connecting to metadata server: %+v", err)
 	}
 
-	statusCodeMsg := "error connecting to metadata server, status code: %d"
-	switch resp.StatusCode {
-	case 404, 412:
-		return resp, fmt.Errorf(statusCodeMsg, resp.StatusCode)
+	if resp == nil {
+		return nil, fmt.Errorf("got nil response from metadata server")
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		defer resp.Body.Close()
+		// Ignore read error as we are returning original error and wrapping MDS error code.
+		r, _ := io.ReadAll(resp.Body)
+		return resp, fmt.Errorf("invalid response from metadata server, status code: %d, reason: %s", resp.StatusCode, string(r))
 	}
 
 	if cfg.hang {
